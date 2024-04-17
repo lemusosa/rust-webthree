79193c6269c7ac202fbc7e91b9dbeecb7fafc463
lemusosa <alvinandwealth@gmail.com>
2024-04-17 06:05:09 +0200

    intro to rust datat types

--- solana-pre/hello_solana/src/main.rs
+++ solana-pre/hello_solana/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // Variables and simple printing
+    let a: i32 = 10;
+    let b: i32 = 15;
+    println!("Hello, world!, {} {}", a,b);
+
+    // Dat types
+
+    // unsigned integer
+    // u8, u16, u32, u64, u128
+    let unsigned: u32 = 10;
+
+    // signed integer
+    // i8, i186, i32, i64, i128
+    let signed: i32 = -10;
+
+    // float is used for decimals: f64 offers more precision
+    let float: f64 = 0.32;
+
+    println!("Different numbers => {} {} {}", unsigned, signed, float);
+
 }
---
+++ solana-pre/hello_solana/target/debug/deps/hello_solana-1bea1275a2106374.d
@@ -0,0 +1,5 @@
+/home/lemuel/Documents/web-3-work-and-projects/rust-webthree-portfolio/solana-pre/hello_solana/target/debug/deps/libhello_solana-1bea1275a2106374.rmeta: src/main.rs
+
+/home/lemuel/Documents/web-3-work-and-projects/rust-webthree-portfolio/solana-pre/hello_solana/target/debug/deps/hello_solana-1bea1275a2106374.d: src/main.rs
+
+src/main.rs:
